d5489937a444de49a0b99612163ec5cce32cfb09
Nodirbek94 <nodirbekmaksudov@yahoo.com>
2020-11-01 10:30:03 -0500

    finished navbar

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MyPortfolio-React
+<img src="src/images/marvin-meyer-SYTO3xs06fU-unsplash.jpg">
---
+++ src/components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeroSection.css';
+import '../App.css';
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            {/* <video src='../videos/video-2.mp4' autoPlay loop muted /> */}
+        </div>
+    )
+}
+
+export default HeroSection
