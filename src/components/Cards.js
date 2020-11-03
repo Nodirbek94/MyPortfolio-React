3855c00e15e6b5c7b0b389edf7b384f7e21d8746
Nodirbek94 <nodirbekmaksudov@yahoo.com>
2020-11-02 21:55:54 -0500

    added portfolio to cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,49 +9,49 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem src="/imgs/11.jpg"
+                        <CardItem src="images/Screen Shot 2020-09-19 at 5.01.00 PM.png"
                             text="For Project 1, we have created app called Local Brews and users can use this app to find local Breweries!"
                             label="Local Brews"
                             path="https://nodirbek94.github.io/Local-Brews/"
                         />
-                        <CardItem src="https://media-cdn.tripadvisor.com/media/photo-s/0e/cc/0a/dc/restaurant-chocolat.jpg"
+                        <CardItem src="images/Screen Shot 2020-10-19 at 4.27.16 PM.png"
                             text="For Project 2, we have created Restaurant Finder where users can use the app to find local Restaurants!"
                             label="Restaurant Finder"
-                            path="/contact"
+                            path="https://findtherestaurant500.herokuapp.com/"
                         />
                     </ul>
                     <ul className="cards__items">
-                        <CardItem src="https://uploads.visitseattle.org/2015/07/01210133/FremontBrewing-SparklyPhotography-CMYK-900x600.jpg"
-                            text="For Project 1, we have created app called Local Brews and users can use this app to find local Breweries!"
-                            label="Project 1"
-                            path="/"
+                        <CardItem src="images/Screen Shot 2020-10-19 at 4.18.05 PM.png"
+                            text="Burger App, user can order new burgers and it can be devoured when the devoured button is pressed!"
+                            label="Burger App"
+                            path="https://hidden-meadow-84053.herokuapp.com/burgers"
                         />
-                        <CardItem src="https://media-cdn.tripadvisor.com/media/photo-s/0e/cc/0a/dc/restaurant-chocolat.jpg"
-                            text="For Project 2, we have created Restaurant Finder where users can use the app to find local Restaurants!"
-                            label="Project 2"
-                            path="/contact"
+                        <CardItem src="images/Screen Shot 2020-10-19 at 4.30.41 PM.png"
+                            text="Note Taker is a app where users can use the app to take new notes and the notes will be saved into the localhost!"
+                            label="Note Taker"
+                            path="https://agile-oasis-44596.herokuapp.com/"
                         />
-                        <CardItem src="https://media-cdn.tripadvisor.com/media/photo-s/0e/cc/0a/dc/restaurant-chocolat.jpg"
-                            text="For Project 2, we have created Restaurant Finder where users can use the app to find local Restaurants!"
-                            label="Project 2"
-                            path="/contact"
+                        <CardItem src="images/Screen Shot 2020-09-20 at 3.34.55 PM.png"
+                            text="Users can use the Work Day Scheduler to organize their work day by saving their meetings or appoitments!"
+                            label="Work Day Scheduler"
+                            path="https://nodirbek94.github.io/WorkDayScheduler/"
                         />
                     </ul>
                     <ul className="cards__items">
-                        <CardItem src="https://uploads.visitseattle.org/2015/07/01210133/FremontBrewing-SparklyPhotography-CMYK-900x600.jpg"
-                            text="For Project 1, we have created app called Local Brews and users can use this app to find local Breweries!"
-                            label="Project 1"
-                            path="/"
+                        <CardItem src="images/Screen Shot 2020-09-20 at 3.39.08 PM.png"
+                            text="Random password can be used to generate random passwords, users can create variety of password with numbers, letters and special characters!"
+                            label="Password Generator"
+                            path="https://nodirbek94.github.io/RandomPG/"
                         />
-                        <CardItem src="https://media-cdn.tripadvisor.com/media/photo-s/0e/cc/0a/dc/restaurant-chocolat.jpg"
-                            text="For Project 2, we have created Restaurant Finder where users can use the app to find local Restaurants!"
-                            label="Project 2"
-                            path="/contact"
+                        <CardItem src="images/Screen Shot 2020-09-20 at 3.50.57 PM.png"
+                            text="This is a quiz application with 10 JavaScript related questions and user have 100 seconds to answer all the questions!"
+                            label="Quiz Code Challenge"
+                            path="https://nodirbek94.github.io/CodeQuiz/"
                         />
-                        <CardItem src="https://media-cdn.tripadvisor.com/media/photo-s/0e/cc/0a/dc/restaurant-chocolat.jpg"
-                            text="For Project 2, we have created Restaurant Finder where users can use the app to find local Restaurants!"
-                            label="Project 2"
-                            path="/contact"
+                        <CardItem src="images/Screen Shot 2020-10-26 at 6.21.37 PM.png"
+                            text="Budget Tracker is application where users can add transaction or subtract transaction and app can be used offline too!"
+                            label="Budget Tracker"
+                            path="https://budget-ap.herokuapp.com/"
                         />
                     </ul>
                 </div>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import '../App.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            {/* <video src='../videos/video-2.mp4' autoPlay loop muted /> */}
+            <video src='/videos/video-2.mp4' autoPlay loop muted />
         </div>
     )
 }
