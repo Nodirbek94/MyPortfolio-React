c242a3ee2ad8a2a4443c683dda2b43c5f79a5037
Nodirbek94 <nodirbekmaksudov@yahoo.com>
2020-11-03 10:04:46 -0500

    added icons to contact page

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -4,13 +4,19 @@ import './ContactMe.css'
 function ContactMe() {
     return (
         <div class="container-fluid border border-light rounded bg-white mb-3 mt-2 w-50 p-auto">
-            <h3 className="display-4 border-bottom">Contact Me</h3>
+            <h1 className="display-4 border-bottom">Contact Me</h1>
+            <div className="icons">
+                <a href="https://github.com/Nodirbek94" target="_blank"><i className="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/nodirbek-maksudov-a74788186/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                <a href="mailto:nodirbekmaksudov@gmail.com" target="_blank"><i className="fas fa-envelope"></i></a>
+                <a href="tel:2675547743" target="_blank"><i className="fas fa-phone-square"></i></a>
+            </div>
             <form className="form">
                 <label>Name</label>
-                <input placeholder="Name"/>
+                <input placeholder="Name" />
 
                 <label>Email</label>
-                <input placeholder="Email"/>
+                <input placeholder="Email" />
 
                 <label>Message</label>
                 <textarea placeholder="Message" />
